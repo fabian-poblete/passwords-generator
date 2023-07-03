aa522319855e87a8227b52b9f929896ce35c1c23
fabian-poblete <fabian.poblete@cftsa.cl>
2023-07-03 14:29:15 -0400

    Proyecto Generador de contraseñas primera fase funcional completado

---
+++ generator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import random
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'generator/index.html')
+
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': generated_password})
